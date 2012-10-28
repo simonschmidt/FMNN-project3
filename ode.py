bff99d6aa6537b3b3f037bb7ebd4c9e839d5421b
Jens Nockert <jens@nockert.se>
2012-10-28 17:49:21 +0100

    Gangnam Style!

--- ode.py
+++ ode.py
@@ -35,16 +35,16 @@ class SecondOrderExplicit_Problem(Explicit_Problem):
             and pass that into Explicit_Problem
             """
 
-            n=len(yyd)/2
+            n=len(yyd) / 2
 
-            dv = rhs(t,yyd[:n],yyd[n:]) if newrhs.isDamped else rhs(t,yyd[:n])
+            dv = rhs(t, yyd[:n], yyd[n:]) if newrhs.isDamped else rhs(t, yyd[:n])
 
-            return numpy.hstack((yyd[n:],dv))
+            return numpy.hstack((yyd[n:], dv))
 
         newrhs.isDamped = isDamped
 
         # need to stack y0 and yd0 together as initial condition for newrhs
-        yyd0 = numpy.hstack((y0,yd0))
+        yyd0 = numpy.hstack((y0, yd0))
 
         super(SecondOrderExplicit_Problem, self).__init__(newrhs, yyd0, t0)
 
@@ -67,48 +67,49 @@ def so_test(rhs=None, y0=None, yd0=None, tfinal=10.0, solver=CVode, arrows=True,
     # Default rhs, note time-dependency
     if rhs is None:
         def rhs(t,y,dy):                
-            A =numpy.array([[0,1],[-2,-1]])
-            B =numpy.array([[-1,2],[-2,0]]) 
-            return numpy.dot(A,y) +numpy.dot(B,(1+numpy.cos(t))*dy)
-    if y0 is None:
-        y0=numpy.array([1,1])
-    if yd0 is None:
-        yd0=numpy.array([-2,1])
+            A =numpy.array([[ 0, 1], [-2, -1]])
+            B =numpy.array([[-1, 2], [-2,  0]])
+
+            return numpy.dot(A, y) + numpy.dot(B, (1.0 + numpy.cos(t)) * dy)
+
+    if y0 is None: y0 = numpy.array([1,1])
+
+    if yd0 is None: yd0 = numpy.array([-2,1])
 
     # Set up model and run a solver
-    model = SecondOrderExplicit_Problem(rhs,y0,yd0)
+    model = SecondOrderExplicit_Problem(rhs, y0, yd0)
 
     sim = solver(model)
 
-    t,y = sim.simulate(tfinal)
-    (y,dy) = (y[:,0:2],y[:,2:])
+    t,  y = sim.simulate(tfinal)
+    y, dy = (y[:, 0:2], y[:, 2:])
 
     # Plot components relative time
-    pylab.subplot(2,1,1)
+    pylab.subplot(2, 1, 1)
     pylab.hold(True)
 
-    pylab.plot(t,y[:,0],label='$y_0$')
-    pylab.plot(t,y[:,1],label='$y_1$')
+    pylab.plot(t, y[:, 0], label='$y_0$')
+    pylab.plot(t, y[:, 1], label='$y_1$')
     pylab.xlabel('t')
     pylab.legend(loc=0)
 
-
     # Plot y₀ relative y₁
-    pylab.subplot(2,1,2)
+    pylab.subplot(2, 1, 2)
     pylab.hold(True)
     pylab.grid(True)
 
-    pylab.plot(y[:,0],y[:,1])
+    pylab.plot(y[:,0], y[:,1])
+
     pylab.xlabel('$y_0$')
     pylab.ylabel('$y_1$')
 
     # Arrows in tangent direction (similar to phase portrait, but only on the solution)
     if arrows:
-        scale = arrow_scaling/numpy.max(map(numpy.linalg.norm,dy))
-        for x,dx in zip(y[::arrow_distance],dy[::arrow_distance]):
-            dx=dx*scale
-            pylab.arrow(x[0],x[1],dx[0],dx[1],head_width=arrow_head_width,alpha=0.8,fc='k')
+        scale = arrow_scaling / numpy.max(map(numpy.linalg.norm, dy))
 
+        for x, dx in zip(y[::arrow_distance], dy[::arrow_distance]):
+            dx = dx * scale
+            pylab.arrow(x[0], x[1], dx[0], dx[1], head_width=arrow_head_width, alpha=0.8, fc='k')
 
     pylab.show()
 
