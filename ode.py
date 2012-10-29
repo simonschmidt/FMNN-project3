cf4598f2f41bc2f84920c4a29b9a7f7cb02453ae
Kjartan Myrdal <kjartankg@gmail.com>
2012-10-29 02:12:37 +0100

    Sofdu unga astin min, uti regnid graetur, vid skulum ekki vaka um dimmar naetur.

--- ode.py
+++ ode.py
@@ -48,72 +48,7 @@ class SecondOrderExplicit_Problem(Explicit_Problem):
 
         super(SecondOrderExplicit_Problem, self).__init__(newrhs, yyd0, t0)
 
-def so_test(rhs=None, y0=None, yd0=None, tfinal=10.0, solver=CVode, arrows=True, arrow_distance=5, arrow_scaling=1., arrow_head_width=0.1):
-    """Function to simplify testing of SecondOrderExplicit_Problem
 
-    rhs: function s.t. y'' = rhs(t,y,y')
-    y0,yd0: initials
-    tfinal: stop time
-    solver: default CVode
-
-    arrows: Plot arrows in solution curve plot
-    arrow_distance: Number of solution points to skip between each arrow
-    arrow_scaling: maximum length of an arrow
-    arrow_head_width:
-
-    returns the solution matrix (t,y,dy)
-    """
-
-    # Default rhs, note time-dependency
-    if rhs is None:
-        def rhs(t, y, dy):
-            A =numpy.array([[ 0, 1], [-2, -1]])
-            B =numpy.array([[-1, 2], [-2,  0]])
-
-            return numpy.dot(A, y) + numpy.dot(B, (1.0 + numpy.cos(t)) * dy)
-
-    if y0 is None: y0 = numpy.array([1,1])
-
-    if yd0 is None: yd0 = numpy.array([-2,1])
-
-    # Set up model and run a solver
-    model = SecondOrderExplicit_Problem(rhs, y0, yd0)
-
-    sim = solver(model)
-
-    t,  y = sim.simulate(tfinal)
-    y, dy = (y[:, 0:2], y[:, 2:])
-
-    # Plot components relative time
-    pylab.subplot(2, 1, 1)
-    pylab.hold(True)
-
-    pylab.plot(t, y[:, 0], label='$y_0$')
-    pylab.plot(t, y[:, 1], label='$y_1$')
-    pylab.xlabel('t')
-    pylab.legend(loc=0)
-
-    # Plot y₀ relative y₁
-    pylab.subplot(2, 1, 2)
-    pylab.hold(True)
-    pylab.grid(True)
-
-    pylab.plot(y[:, 0], y[:, 1])
-
-    pylab.xlabel('$y_0$')
-    pylab.ylabel('$y_1$')
-
-    # Arrows in tangent direction (similar to phase portrait, but only on the solution)
-    if arrows:
-        scale = arrow_scaling / numpy.max(map(numpy.linalg.norm, dy))
-
-        for x, dx in zip(y[::arrow_distance], dy[::arrow_distance]):
-            dx = dx * scale
-            pylab.arrow(x[0], x[1], dx[0], dx[1], head_width=arrow_head_width, alpha=0.8, fc='k')
-
-    pylab.show()
-
-    return (t,y,dy)
 
 # Used src/solvers/euler.pyx to figure out behaviour
 # Could use some touching up, like get/set functions for g,b like h 
@@ -266,6 +201,10 @@ class Newmark(ExplicitEuler):
 
 class HHT(Explicit_ODE):
     def __init__(self, problem, alpha = 0.0):
+        """
+            problem: assimulo Explicit_Probelm
+            alpha: optional value on the interval [-1/3,0]
+        """
         Explicit_ODE.__init__(self, problem)
 
         self.options["a"] = alpha
@@ -283,8 +222,6 @@ class HHT(Explicit_ODE):
 
         self.supports["one_step_mode"] = True
 
-        #problem.rhs expected to be rhs(t,y) = y'
-        #where y = hstack((p,v)) and y' = hstack((v,a))
         self.rhs = problem.rhs
         self.n = len(self.y0) / 2
 
@@ -300,6 +237,9 @@ class HHT(Explicit_ODE):
             return assimulo.ode.ID_PY_COMPLETE, t, y
         
     def _step(self, t, y, h):
+        """
+        Internal, don't use directly. Use function step instead
+        """
 
         if self.a_old is None: self.a_old = self.rhs(t,y)
         
@@ -358,7 +298,7 @@ class HHT(Explicit_ODE):
 
     def _hhtError(self, y, t, h, a_new):
         """
-            Wrong: a_new - rhs(t_new, p_new, v_new)
+            a_new - ((1 + alpha)rhs(t_new, p_new, v_new) - alpha rhs(t,p,v))
         """
         y_new = self._hht(y,h,a_new)
         alpha = self.a
@@ -421,6 +361,73 @@ def truck_test(solver=Newmark, tfinal=60.0):
     pylab.show()
 
     return ((nt,ny), (ct,cy))
+    
+def so_test(rhs=None, y0=None, yd0=None, tfinal=10.0, solver=CVode, arrows=True, arrow_distance=5, arrow_scaling=1., arrow_head_width=0.1):
+    """Function to simplify testing of SecondOrderExplicit_Problem
+
+    rhs: function s.t. y'' = rhs(t,y,y')
+    y0,yd0: initials
+    tfinal: stop time
+    solver: default CVode
+
+    arrows: Plot arrows in solution curve plot
+    arrow_distance: Number of solution points to skip between each arrow
+    arrow_scaling: maximum length of an arrow
+    arrow_head_width:
+
+    returns the solution matrix (t,y,dy)
+    """
+
+    # Default rhs, note time-dependency
+    if rhs is None:
+        def rhs(t, y, dy):
+            A =numpy.array([[ 0, 1], [-2, -1]])
+            B =numpy.array([[-1, 2], [-2,  0]])
+
+            return numpy.dot(A, y) + numpy.dot(B, (1.0 + numpy.cos(t)) * dy)
+
+    if y0 is None: y0 = numpy.array([1,1])
+
+    if yd0 is None: yd0 = numpy.array([-2,1])
+
+    # Set up model and run a solver
+    model = SecondOrderExplicit_Problem(rhs, y0, yd0)
+
+    sim = solver(model)
+
+    t,  y = sim.simulate(tfinal)
+    y, dy = (y[:, 0:2], y[:, 2:])
+
+    # Plot components relative time
+    pylab.subplot(2, 1, 1)
+    pylab.hold(True)
+
+    pylab.plot(t, y[:, 0], label='$y_0$')
+    pylab.plot(t, y[:, 1], label='$y_1$')
+    pylab.xlabel('t')
+    pylab.legend(loc=0)
+
+    # Plot y₀ relative y₁
+    pylab.subplot(2, 1, 2)
+    pylab.hold(True)
+    pylab.grid(True)
+
+    pylab.plot(y[:, 0], y[:, 1])
+
+    pylab.xlabel('$y_0$')
+    pylab.ylabel('$y_1$')
+
+    # Arrows in tangent direction (similar to phase portrait, but only on the solution)
+    if arrows:
+        scale = arrow_scaling / numpy.max(map(numpy.linalg.norm, dy))
+
+        for x, dx in zip(y[::arrow_distance], dy[::arrow_distance]):
+            dx = dx * scale
+            pylab.arrow(x[0], x[1], dx[0], dx[1], head_width=arrow_head_width, alpha=0.8, fc='k')
+
+    pylab.show()
+
+    return (t,y,dy)
 
 if __name__ == "__main__":
     print " --- Pendulum --- "
